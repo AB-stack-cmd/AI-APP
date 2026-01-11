e6c42b46d545c4f3e95df796526458772bd569da
Abir00009 <abirsarkar123455@gamil.com>
2026-01-11 11:22:56 +0530

    auth added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
 
 export default function Home() {
   return (
-      <h1>HEllo</h1>
+    <>
+      <h1>HEllo</h1> <Button onClick={() => alert("Clicked")}>Click me</Button>
+      <UserButton/>
+    </>
   );
 }
